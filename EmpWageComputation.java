ff70d5364326b5659b620458d590508104b26f2b
Rutujakalyane <60542373+Rutujakalyane@users.noreply.github.com>
2020-04-17 15:52:25 +0530

    REFACTORED :  Using Class Method to Compute Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,17 +1,25 @@
 public class EmpWageComputation {
 	//CONSTANTS
-	final int IS_PART_TIME = 1;
-	final int IS_FULL_TIME = 2;
-	final int EMP_RATE_PER_HOUR = 20;
-	final int NUM_OF_WORKING_DAYS = 20;
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
+
+	public static int calTotalEmpWage( int totalEmpHrs )	{
+		return totalEmpHrs * EMP_RATE_PER_HOUR;
+	}
 
 	public static void main(String args[]) {
 		//VARIABLES
-		int empHrs = 0, empWage = 0, totalEmpWage = 0;
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 		//COMPUTATION
-		for ( int day = 1; day <= NUM_OF_WORKING_DAYS; day++ ) {
+		while ( totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS )
+		{
+			totalWorkingDays++;
 			int empCheck = (int) ( Math.random() * 10 ) % 3;
-			switch (empCheck)	{
+			switch (empCheck)
+			{
 				case IS_PART_TIME:
 					empHrs = 4;
 					break;
@@ -21,10 +29,10 @@ public class EmpWageComputation {
 				default:
 					empHrs = 0;
 			}
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
-			System.out.println("Emp Wage for Day " + day +": "+ empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +  empHrs);
 		}
+		int totalEmpWage = calTotalEmpWage( totalEmpHrs );
 		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 }
